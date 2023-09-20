0b5739e85ac2958534b6c730c85bb9f8df3ecdfd
Braian Quispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2023-09-20 07:19:05 -0300

    chore(api): :hammer: add param to fetch class

--- server/src/libs/fetch.js
+++ server/src/libs/fetch.js
@@ -5,11 +5,12 @@ const https = require("https");
  */
 class FetchApi {
   /**
+   * @template T
    * @param {string} url
    * @param {import('https').RequestOptions} [options]
-   * @returns {Promise<unknown>}
+   * @returns {Promise<T>}
    */
-  static get(url, options) {
+  static get(url, options, parseJSON = true) {
     return new Promise((resolve, reject) => {
       https
         .get(url, options, (response) => {
@@ -20,8 +21,11 @@ class FetchApi {
           });
 
           response.on("end", () => {
-            const jsonData = JSON.parse(data);
-            resolve(jsonData);
+            if (parseJSON) {
+              const jsonData = JSON.parse(data);
+              return resolve(jsonData);
+            }
+            resolve(data);
           });
         })
         .on("error", (err) => {
