74c84f97cb8d32e0c7fb913d7143bdde7be5b9b8
Braian Quispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2023-09-21 20:02:45 -0300

    fix(api): :bug: change module export

--- server/src/errors/http-error.js
+++ server/src/errors/http-error.js
@@ -1,6 +1,8 @@
-export class HttpError extends Error {
+class HttpError extends Error {
   constructor(status = 500, message) {
     this.status = status;
     this.message = message;
-  } 
+  }
 }
+
+module.exports = HttpError;
