fbbf5c75e73ef6cc82faf956bfa6273f92aa1700
Braian Quispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2023-09-23 09:05:57 -0300

    chore(api): :wrench: add headers config

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,13 @@ require('./files/dependencies')
 const app = express()
 app.use(express.json())
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  next();
+})
+
 app.use(routes)
 app.use(errorHandler)
 
